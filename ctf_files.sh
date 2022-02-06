5f36190a94ae3d78ee3dcfb0a85e63dc48e3a3e8
Namx <namx@localhost.localdomain>
2022-02-06 21:47:39 +0530

    new files

--- ctf_files.sh
+++ ctf_files.sh
@@ -1,37 +1,5 @@
 echo "These are all files for CTFs"
 
-#Installing nmap
-sudo apt install nmap
-
-#Installing wordlists
-mkdir /opt/wordlists
-cd /opt/wordlists
-wget https://github.com/brannondorsey/naive-hashcat/releases/download/data/rockyou.txt
-wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Discovery/Web-Content/directory-list-lowercase-2.3-medium.txt
-
-#Installing gobuster 
-sudo apt install gobuster 
-
-#installing dirbuster
-sudo apt install dirbuster
-
-#installing hydra
-sudo apt install hydra
-
-#installing john_the-Ripper
-cd /opt 
-git clone https://github.com/openwall/john -b bleeding-jumbo john
-cd john/src/
-./confiigure
-make -s clean && make -sj4
-cd ../run
-./john --test 
-
-#CLoning linPEAS
-git clone https://github.com/carlospolop/PEASS-ng/tree/master/linPEAS
-
-#Cloning winPEAS
-git clone https://github.com/carlospolop/PEASS-ng/tree/master/winPEAS
 
 #installing enum4linux
 git clone https://github.com/CiscoCXSecurity/enum4linux.git
