d91a00b0d1e70c4b95eedcd289ab5e03575217cf
naman <namx05@gmail.com>
2022-03-15 18:22:48 +0530

    new changes

--- ctf_files.sh
+++ ctf_files.sh
@@ -6,6 +6,6 @@ git clone https://github.com/CiscoCXSecurity/enum4linux.git
 
 #Installing RustScan
 wget https://github.com/RustScan/RustScan/releases/download/2.0.1/rustscan_2.0.1_amd64.deb
-dpkg -i rustscan_2.0.1_amd64.deb
-rm -rfv rustscan_2.0.1_amd64.debe
+sudo dpkg -i rustscan_2.0.1_amd64.deb
+sudo rm -rfv rustscan_2.0.1_amd64.debe
 
